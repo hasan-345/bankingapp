233f7b8bd277e498fb62e56fe606288cd608f433
Ahtisham-ul-hassan <ahtishamhassan345@gmail.com>
2024-06-20 13:49:34 +0500

    web

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing"
+import Button from "./Button"
+import CardDeal from "./CardDeal"
+import CTA from "./CTA"
+import FeedBackCard from "./FeedBackCard"
+import Footer from "./Footer"
+import GetStarted from "./GetStarted"
+import Testimonials from "./Testimonials";
+import Stats from "./Stats";
+import Clients from "./Clients"
+import Business from "./Business"
+import Hero from "./Hero"
+
+export {
+    Navbar,
+    Billing,
+    Button,
+    CardDeal,
+    CTA,
+    FeedBackCard,
+    Footer,
+    GetStarted,
+    Testimonials,
+    Stats,
+    Clients,
+    Business,
+    Hero
+}
